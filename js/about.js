e4b3dc3f0387baeba587f4a24c6a332114de3306
wuwr4 <wuwr@uci.edu>
2026-02-01 00:08:59 -0800

    Completed Tweet dates, Tweet categories, and User-written tweets

--- js/about.js
+++ js/about.js
@@ -1,20 +1,107 @@
+
 function parseTweets(runkeeper_tweets) {
-	//Do not proceed if no tweets loaded
+
+	// Do not proceed if no tweets loaded
 	if(runkeeper_tweets === undefined) {
 		window.alert('No tweets returned');
 		return;
 	}
 
+	// An array of the retrieved tweets
 	tweet_array = runkeeper_tweets.map(function(tweet) {
 		return new Tweet(tweet.text, tweet.created_at);
 	});
+
+	let num_tweets = tweet_array.length;
 	
-	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
-	//It works correctly, your task is to update the text of the other tags in the HTML file!
-	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	// This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
+	// It works correctly, your task is to update the text of the other tags in the HTML file!
+	document.getElementById('numberTweets').innerText = num_tweets;	
+
+	// Tweet dates (1pt)
+
+		// Options to format the date string
+		const options = {
+			weekday: "long",
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+		};
+
+		// Update the earliest date
+		document.getElementById('firstDate').innerHTML = tweet_array[tweet_array.length - 1].time.toLocaleDateString("en-US", options);
+
+		// Update the latest date
+		document.getElementById('lastDate').innerHTML = tweet_array[0].time.toLocaleDateString("en-US", options);
+
+	// Tweet Categories (1pt)
+
+		// Calculate the counts and percentages
+		let num_completed_events = 0;
+		let num_live_events = 0;
+		let num_achievements = 0;
+		let num_miscellaneous = 0;
+
+		let num_written = 0;
+
+		for(const tweet of tweet_array) {
+			switch(tweet.source) {
+				case "completed_event":
+					num_completed_events++;
+					break;
+				
+				case "live_event":
+					num_live_events++;
+					break;
+				
+				case "achievement":
+					num_achievements++;
+					break;
+				
+				case "miscellaneous":
+					num_miscellaneous++;
+					break;
+			}
+
+			if(tweet.written) {
+				num_written++;
+			}
+		}
+
+		const formatting = {
+			notation: "fixed",
+			precision: 2
+		};
+
+		let percent_completed_events = math.format(num_completed_events / num_tweets * 100, formatting); 
+		let percent_live_events = math.format(num_live_events / num_tweets * 100, formatting); 
+		let percent_achievements = math.format(num_achievements / num_tweets * 100, formatting); 
+		let percent_miscellaneous = math.format(num_miscellaneous / num_tweets * 100, formatting); 
+
+		let percent_written = math.format(num_written / num_completed_events * 100, formatting);
+
+		// Update the HTML
+		document.getElementsByClassName("completedEvents")[0].innerHTML = num_completed_events;
+		document.getElementsByClassName("completedEventsPct")[0].innerHTML = percent_completed_events + "%";
+
+		document.getElementsByClassName("liveEvents")[0].innerHTML = num_live_events;
+		document.getElementsByClassName("liveEventsPct")[0].innerHTML = percent_live_events + "%";
+
+		document.getElementsByClassName("achievements")[0].innerHTML = num_achievements;
+		document.getElementsByClassName("achievementsPct")[0].innerHTML = percent_achievements + "%";
+
+		document.getElementsByClassName("miscellaneous")[0].innerHTML = num_miscellaneous;
+		document.getElementsByClassName("miscellaneousPct")[0].innerHTML = percent_miscellaneous + "%";
+ 	
+	// User-written tweets (1pt)
+	document.getElementsByClassName("completedEvents")[1].innerHTML = num_completed_events;
+	document.getElementsByClassName("written")[0].innerHTML = num_written;
+	document.getElementsByClassName("writtenPct")[0].innerHTML = percent_written + "%";
 }
 
-//Wait for the DOM to load
+// Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function (event) {
+
+	// Gets the tweets, then parses it
 	loadSavedRunkeeperTweets().then(parseTweets);
 });
--- ts/tweet.ts
+++ ts/tweet.ts
@@ -1,50 +1,68 @@
-class Tweet {
-	private text:string;
-	time:Date;
 
-	constructor(tweet_text:string, tweet_time:string) {
+class Tweet {
+	
+    private text: string;
+	time: Date;
+
+	constructor(tweet_text: string, tweet_time: string) {
         this.text = tweet_text;
 		this.time = new Date(tweet_time);//, "ddd MMM D HH:mm:ss Z YYYY"
 	}
 
 	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
-    get source():string {
-        //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        return "unknown";
+    get source(): string {
+        if(this.text.startsWith("Just ") && (this.text.includes("completed") || this.text.includes("posted"))) {
+            return "completed_event"
+        }
+
+        if(this.text.startsWith("Achieved ") && this.text.includes("personal") && this.text.includes("record")) {
+            return "achievement"
+        }
+
+        if(this.text.startsWith("Watch ") && this.text.includes("#RKLive")) {
+            return "live_event"
+        }
+
+        return "miscellaneous";
     }
 
-    //returns a boolean, whether the text includes any content written by the person tweeting.
-    get written():boolean {
-        //TODO: identify whether the tweet is written
+    // returns a boolean, whether the text includes any content written by the person tweeting.
+    get written(): boolean {
+        // TODO: identify whether the tweet is written
+
+        if(this.text.includes("-")) {
+            return true;
+        }
+
         return false;
     }
 
-    get writtenText():string {
+    get writtenText(): string {
         if(!this.written) {
             return "";
         }
-        //TODO: parse the written text from the tweet
+        // TODO: parse the written text from the tweet
         return "";
     }
 
-    get activityType():string {
+    get activityType(): string {
         if (this.source != 'completed_event') {
             return "unknown";
         }
-        //TODO: parse the activity type from the text of the tweet
+        // TODO: parse the activity type from the text of the tweet
         return "";
     }
 
-    get distance():number {
+    get distance(): number {
         if(this.source != 'completed_event') {
             return 0;
         }
-        //TODO: prase the distance from the text of the tweet
+        // TODO: prase the distance from the text of the tweet
         return 0;
     }
 
     getHTMLTableRow(rowNumber:number):string {
-        //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
+        // TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
         return "<tr></tr>";
     }
 }
